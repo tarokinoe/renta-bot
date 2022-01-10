f9cf5b6c489d1d3874c92e6edc790d56be1a295b
Kintaro Oe <tarokinoe@gmail.com>
2022-01-10 21:57:46 +0300

    refactoring: Poetry, new lib versions, xpath

--- rentabot/rentabot/settings.py
+++ rentabot/rentabot/settings.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-
 # Scrapy settings for rentabot project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
 #
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'rentabot'
 
@@ -19,13 +17,13 @@ NEWSPIDER_MODULE = 'rentabot.spiders'
 #USER_AGENT = 'rentabot (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
-# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
@@ -45,31 +43,31 @@ ROBOTSTXT_OBEY = False
 #}
 
 # Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'rentabot.middlewares.MyCustomSpiderMiddleware': 543,
+#    'rentabot.middlewares.RentabotSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'rentabot.middlewares.MyCustomDownloaderMiddleware': 543,
+#    'rentabot.middlewares.RentabotDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
 # Configure item pipelines
-# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'rentabot.pipelines.SomePipeline': 300,
+#    'rentabot.pipelines.RentabotPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
-# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
@@ -82,7 +80,7 @@ ROBOTSTXT_OBEY = False
 #AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 #HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
--- rentabot/rentabot/spiders/snimaemsami.py
+++ rentabot/rentabot/spiders/snimaemsami.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import scrapy
 import json
@@ -7,41 +8,47 @@ import time
 # Telegram bot
 BOT_TOKEN = ""
 bot = telegram.Bot(token=BOT_TOKEN)
-CHAT_ID = None
-
-# 
-PARSED_APPARTMENTS = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                  'parsed_appartments.txt')
-
-SITE_URL = "http://snimaem-sami.ru"
-START_PAGE = SITE_URL + '/objects/?fieldsfilter%5Bobject_type%5D%5B%5D=802508&fieldsfilter%5Bobject_type%5D%5B%5D=574&fieldsfilter%5Broom_count_sidebar%5D%5B%5D=room1&fieldsfilter%5Broom_count_sidebar%5D%5B%5D=&fieldsfilter%5Broom_count_sidebar%5D%5B%5D=&fieldsfilter%5Broom_count_sidebar%5D%5B%5D=&fieldsfilter%5Bstation%5D%5B%5D=667&fieldsfilter%5Bstation%5D%5B%5D=831&fieldsfilter%5Bstation%5D%5B%5D=663&fieldsfilter%5Bstation%5D%5B%5D=691&fieldsfilter%5Bstation%5D%5B%5D=665&fieldsfilter%5Bstation%5D%5B%5D=671&fieldsfilter%5Bstation%5D%5B%5D=925&fieldsfilter%5Bstation%5D%5B%5D=808&fieldsfilter%5Bstation%5D%5B%5D=806&fieldsfilter%5Bstation%5D%5B%5D=882&fieldsfilter%5Bstation%5D%5B%5D=901&fieldsfilter%5Bstation%5D%5B%5D=618&fieldsfilter%5Bstation%5D%5B%5D=614&fieldsfilter%5Bprice%5D%5B0%5D=0&fieldsfilter%5Bprice%5D%5B1%5D=22000'
+CHAT_ID =
+SITE_URL = "https://snimaem-sami.ru"
+START_PAGE = SITE_URL + '/objects/?fieldsfilter%5Bobject_type%5D%5B0%5D=802508&fieldsfilter%5Bobject_type%5D%5B1%5D=574&fieldsfilter%5Broom_count_sidebar%5D%5B0%5D=room1&fieldsfilter%5Broom_count_sidebar%5D%5B1%5D=&fieldsfilter%5Broom_count_sidebar%5D%5B2%5D=&fieldsfilter%5Broom_count_sidebar%5D%5B3%5D=&fieldsfilter%5Bprice%5D%5B0%5D=0&fieldsfilter%5Bprice%5D%5B1%5D=505000&fieldsfilter%5Bstation%5D%5B0%5D=921'
 PARSING_PERIOD = 100
+PARSED_APPARTMENTS_FILE = ''
 
 
-appartments = set()
+logger = logging.getLogger(__name__)
 
 
-class SnimaemSami(scrapy.Spider):
-    name = "snimaem-sami"
+class SnimaemsamiSpider(scrapy.Spider):
+    name = 'snimaem-sami'
+    # allowed_domains = ['snimaem-sami.ru']
+    # start_urls = ['https://snimaem-sami.ru/']
+
+    def __init__(self):
+        super().__init__()
+        with open(PARSED_APPARTMENTS_FILE) as f:
+            self._appartments = set(map(str.strip, f))
+
+    def _save_parsed(self, url):
+        with open(PARSED_APPARTMENTS_FILE, 'a') as f:
+            f.write(url + '\n')
+
+        self._appartments.add(url)
 
     def start_requests(self):
-        with open(PARSED_APPARTMENTS) as f:
-            for line in f:
-                appartments.add(line.strip())
-
         yield scrapy.Request(url=START_PAGE, callback=self.parse)
 
-    def parse(self, response):
-        objects = response.css(".middle .inlist_object")
-        for obj in objects:
-            href = obj.css(".further_object > a::attr(href)").extract_first()
-            if href not in appartments:
-                with open(PARSED_APPARTMENTS, 'a') as f:
-                    f.write(href + '\n')
-                appartments.add(href)
+    def parse(self, response, **kwargs):
+        snippets = response.xpath('//div[@class="info_object"]')
+        for snippet in snippets:
+            href = snippet.xpath('./h3/a/@href').get()
+            if href and href not in self._appartments:
+                # self._save_parsed(href)
+                logger.info(f'New appartments: {href}')
                 bot.sendMessage(CHAT_ID, SITE_URL + href)
                 time.sleep(0.3)
-        
+
         time.sleep(PARSING_PERIOD)
 
-        yield scrapy.Request(url=response.url, callback=self.parse, dont_filter=True)
+        yield scrapy.Request(
+            url=response.url, callback=self.parse, dont_filter=True
+        )
